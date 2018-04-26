5b95356ccd75d2ac061421999bda39b4bf774fae
monkeydkon <kon_fu2@hotmail.com>
2018-04-26 21:57:59 +0300

    first try. put useless animation

--- app/src/main/java/com/nplab/monkeydkon/ekpaideutikologismikofinalproject/MainActivity.java
+++ app/src/main/java/com/nplab/monkeydkon/ekpaideutikologismikofinalproject/MainActivity.java
@@ -2,12 +2,22 @@ package com.nplab.monkeydkon.ekpaideutikologismikofinalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+
+        textView.setAlpha(0f);
+
+        textView.animate().alpha(1f).setDuration(500);
     }
 }
