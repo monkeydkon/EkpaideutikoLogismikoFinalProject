87d65839d57c957c70c8db03fc681a18bf19c50f
monkeydkon <kon_fu2@hotmail.com>
2018-05-16 16:27:33 +0300

    statements basic activity done

--- app/src/main/java/com/nplab/monkeydkon/ekpaideutikologismikofinalproject/StatementsActivity.java
+++ app/src/main/java/com/nplab/monkeydkon/ekpaideutikologismikofinalproject/StatementsActivity.java
@@ -2,6 +2,7 @@ package com.nplab.monkeydkon.ekpaideutikologismikofinalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StatementsActivity extends AppCompatActivity {
 
@@ -9,5 +10,29 @@ public class StatementsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statements);
+    }
+    
+    public void ifs(View view){
+        // TODO: 16/5/2018  
+    }
+    
+    public void elseifs(View view){
+        // TODO: 16/5/2018  
+    }
+    
+    public void switches(View view){
+        // TODO: 16/5/2018  
+    }
+    
+    public void whiles(View view){
+        // TODO: 16/5/2018  
+    }
+    
+    public void fors(View view){
+        // TODO: 16/5/2018  
+    }
+    
+    public void quiz(View view){
+        // TODO: 16/5/2018  
     }
 }
---
+++ app/src/main/java/com/nplab/monkeydkon/ekpaideutikologismikofinalproject/StatementsIfsActivity.java
@@ -0,0 +1,16 @@
+package com.nplab.monkeydkon.ekpaideutikologismikofinalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StatementsIfsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statements_ifs);
+    }
+
+    // TODO: 16/5/2018 todo todo todo
+    
+}
