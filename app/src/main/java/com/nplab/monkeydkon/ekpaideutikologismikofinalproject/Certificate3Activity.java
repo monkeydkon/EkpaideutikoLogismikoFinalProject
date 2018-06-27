13b95a7d8e91983afe19cd82ad95d31c4299b5da
Makjin <geomak13@yahoo.gr>
2018-06-27 15:22:46 +0300

    test mak

--- app/src/main/java/com/nplab/monkeydkon/ekpaideutikologismikofinalproject/Certificate3Activity.java
+++ app/src/main/java/com/nplab/monkeydkon/ekpaideutikologismikofinalproject/Certificate3Activity.java
@@ -15,6 +15,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class Certificate3Activity extends AppCompatActivity {
+    
+    //jbhgyuhgutyfgyui
 
     String username;
 
